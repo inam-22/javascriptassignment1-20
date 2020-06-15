2d1548f60ed42d9def38c5c82a8108029c5476b0
inam-22 <inamahmed70@@gmail.com>
2020-06-15 23:24:55 +0500

    Ready to deployed JS Assignment

---
+++ app.js
@@ -0,0 +1,289 @@
+//==== Chapter1 and Task1
+//alert("Hello World!");
+
+//====Chapter1 and Task2
+//alert("Error!Please enter a valid password");
+
+//====Chapter1 and Task3
+//alert("Welcome to JS Land..." + "\n" + "Happy Coding!" );
+
+//====Chapter1 and Task4
+//====Chapter1 and Task5
+//alert("Hello...I can run JS through my web browser's console");
+
+//====Chapter2 and Task1
+// var username;
+
+//====Chapter2 and Task2
+//var myName="Syed Muhammad Inam Ahmed"
+//document.write(myName);
+//var message="Hello World"
+//alert(message);
+//====Chapter2 and Task6
+//var email="example@exacmple.com";
+//alert("My email address is:" + email);
+//====Chapter2 and Task7
+//var book="A Smarter Way to Learn JavaScript";
+//alert("I am trying to learn from the Book" + " " +book);
+//====Chapter2 and Task8
+//document.write("Yah!I can write HTML content through JavaScript");
+
+//====Chapter2 and Task9
+    //var a= '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+    //alert (a);
+   //====Chapter3and Task1 
+   //var Age=15;
+   //alert("I am"+ " "+ Age+ " "+ "Years Old")
+   //====Chapter3and Task3
+   //var birthYear= "1998";
+   //document.write("My birth year is" + birthYear+ "<br/>" +"Data type of my declared variable is number")
+   //====Chapter3and Task1 
+   //var Visitors_name="John Doe";
+   //var product_Title="T-shirts";
+   //var quantity="5"; 
+   //document.write(Visitors_name + " "+ "ordered" + " "+ quantity+product_Title + " "+ "(s)on xyz clothing store") 
+    //====Chapter4and Task1
+    // var a="Hello World"; 
+    //     b=20;
+    //     c="testing";
+    //====Chapter4and Task2
+     //illegal: (1) var my name (2) var if (not keyword) (3) var 1name
+      //(4) var rose="abc" then we cant use Rose instead of rose it's case sensitive 
+     (5) //var %name
+     //leggal  var &name , var _name , var name , _1name , var my_Name
+     //====Chapter4and Task3(a)
+     //document.write ("<h1>Rules for naming JS variables</h1>");
+     //====Chapter4and Task3(b)
+     //A variable name can contain only letters, numbers, dollar signs, and underscores.
+     //====Chapter4and Task3(c)
+     // A variable must begin with $name, _name or name
+     //====Chapter4and Task3(d)
+     //Variable names are case ___sensitive______ 
+     //====Chapter4and Task3(e) 
+     //Variable names should not be JS ___keyword______ 
+     //====Chapter5and Task1
+    // var a=+prompt("Enter First Number");
+    // var b=+prompt("Enter Second Number");
+    // var c=a+b;
+    // document.write("Sum of"+ " "+ a + "and"+ " " + b+ " " +"is" + " "+ c);
+    //====Chapter5and Task2
+    // var a=+prompt("Enter First Number");
+    // var b=+prompt("Enter Second Number");
+    // var c=a-b;
+    // document.write("Subtraction of"+ " "+ a + "and"+ " " + b+ " " +"is" + " "+ c);
+    // var a=+prompt("Enter First Number");
+    // var b=+prompt("Enter Second Number");
+    // var c=a*b;
+    // document.write("Multiplication of"+ " "+ a + "and"+ " " + b+ " " +"is" + " "+ c);
+    // var a=+prompt("Enter First Number");
+    // var b=+prompt("Enter Second Number");
+    // var c=a%b;
+    // document.write("Modulus of"+ " "+ a + "and"+ " " + b+ " " +"is" + " "+ c);
+    //====Chapter5and Task3 (a,b,c,d,e,f,g,h,i,j,k,l)
+        //     var num;
+        //     document.write("Value after variable declaration"+ " " +num +"<br/>");
+        //    num=5;
+        //    document.write("Initial value:"+ " " +num + "<br/>");
+        //    num++;
+        //    document.write("Value of Increment is:"+ " " +num + "<br/>");
+        //    num= num+7;
+        //    document.write("Value after addition is:"+ " " +num + "<br/>");
+        //    num--;
+        //    document.write("Value after decrement is:"+ " " +num + "<br/>");
+        //    num%3;
+        //    document.write("The remainder is:"+ " " +num );
+        //====Chapter5and Task4
+        // var cost_Of_1_Ticket=600;
+        // var cost_Of_5_Ticket=cost_Of_1_Ticket*5;
+        // document.write("Total cost to buy 5 tickets to a movie is" +" "+cost_Of_5_Ticket + "PKR");
+           //====Chapter5and Task5
+        //    document.write("<h1> Table Of 4 </h1>")
+        //    var i=4;
+        //    for( j=1; j<=10; j++){
+        //          document.write(i+ "x"+ j+ "=" + i*j);
+        //          document.write("<br>");
+        //    }
+         //====Chapter5and Task6
+        //  var celciusTemp=25;
+        //  var cel_TO_Far=(celciusTemp*9/5)+32;
+        //  document.write(celciusTemp + "C is" + " "+cel_TO_Far + "F" +"<br/>");
+        //  var far_Temp= 70;
+        //  var far_To_Cel=((far_Temp-32)*5/9);
+        //  document.write(far_Temp + "F is" + " "+far_To_Cel + "F");
+
+         //====Chapter5and Task7
+         //document.write("<h1> Shopping Cart </h1>")
+        //  var pri_Of_item1=650;
+        //  var pri_Of_item2=100;
+        //  var ord_Quant_ofitem1=3;
+        //  var ord_Quant_ofitem2=7;
+        //  var ship_Charges=100;
+        //  document.write("Price of item1 is" + pri_Of_item1 + "<br>");
+        //  document.write("Quantity of item1 is" + ord_Quant_ofitem1 + "<br>");
+        //  document.write("Price of item2 is" + pri_Of_item2 + "<br>");
+        //  document.write("Quantity of item2 is" + ord_Quant_ofitem2 + "<br>");
+        //  document.write("Shipping Charges " + ship_Charges + "<br>"+ "<br>");
+        //  document.write("Total cost of your order is" + (pri_Of_item1*ord_Quant_ofitem1+pri_Of_item2*ord_Quant_ofitem2+ship_Charges));
+        
+         //====Chapter5and Task8
+        //  document.write("<h1> MARK SHEET </h1>")
+        //  var total_Marks=980;
+        //  var obtained_Marks=804;
+        //  var percentage;
+        //  document.write("Total Marks:" + total_Marks+ "<br>");
+        //  document.write("Obtained Marks:" + obtained_Marks+ "<br>");
+        //  document.write("Percentage" + (obtained_Marks/total_Marks)*100);
+
+         //====Chapter5and Task9
+        //  document.write("<h1> CURRENCY IN PKR </h1>")
+        //  var one_US_dollar= 104.80;
+        //  var one_Saudi_Riyal= 28;
+        //  var tot_In_pak_Cur= one_US_dollar*10 + one_Saudi_Riyal*25;
+        //  document.write("Total Currency in PKR:" + tot_In_pak_Cur);
+
+        //====Chapter5and Task10
+        //  var d="5";
+        //  (d=d+5, d=d*5 ,d=d/2);
+        //  document.write(d);
+
+        //====Chapter5and Task11
+        // document.write("<h1> AGE CALCULATOR </h1>")
+        // var birth_Year=1998;
+        // var current_Year=2020;
+        // var current_Age= current_Year-birth_Year;
+        // document.write( "Your Age is:"+current_Age);
+
+        //====Chapter5and Task13
+        // document.write("<h1> THE LIFTIME SUPPLY CALCULATOR </h1>")
+        // var fav_snack="LAYS"
+        // var current_Age=15;
+        // var estimate_Age=65;
+        // var per_Day_snack=3;
+        // var total_snacks_req= (estimate_Age-current_Age)*365;
+        // document.write( "You will need" + " "+total_snacks_req+ " "+ "LAYS to last you untill the ripe old age of 65");
+
+         //====Chapter6 to 9 and Task1
+        //  var e=10;
+        //  document.write("The value of e is:" + e + "<br/>" + "<br/>");
+        //  ++e;
+        //  document.write("The value of ++e is:" + e + "<br/>" );
+        //  document.write(" Now The value of e is:" + e + "<br/>" + "<br/>" );
+        //  document.write("  The value of e++ is:" + e + "<br/>"  );
+        //  e++;
+        //  document.write(" Now The value of e is:" + e + "<br/>" + "<br/>" );
+        //  --e;
+        //  document.write(" The value of --e is:" + e + "<br/>"  );
+        //  document.write(" Now The value of e is:" + e + "<br/>" + "<br/>" );
+        //  document.write(" The value of e-- is:" + e + "<br/>" );
+        //  e--;
+        //  document.write(" The value of e is:" + e + "<br/>" );
+
+          //====Chapter6 to 9 and Task2
+        //   var a=2,b=1;
+        //   document.write("value of a:" + a + "<br/>");
+        //      document.write("value of b:" + b + "<br/>");
+        //   var result= --a - --b + ++b + b--;
+        //      document.write("Result:" + result);
+
+        //====Chapter6 to 9 and Task3
+        // var name=prompt("Enter Your Name");
+        // alert("Hello"+ " "+ name);
+
+        //====Chapter6 to 9 and Task5
+    
+        // var number=prompt("Enter Table Number");
+        // if(number>0){
+        //    for( j=1; j<=10; j++){
+        //           document.write(number+ "x"+ j+ "=" + number*j);
+        //           document.write("<br>");
+        //     }
+        // }
+        // else{
+        //     number=5;
+        //     for( j=1; j<=10; j++){
+        //         document.write(number+ "x"+ j+ "=" + number*j);
+        //         document.write("<br>");
+        //   }
+        // }
+
+        //====Chapter 9 to 11 and Task1
+        // var city=prompt("Enter city name")
+        //   city =city.toLowerCase();
+        // if (city==="karachi"){
+        //     alert("Welcome to City Of Lights");
+        // }
+        // else{
+        //     alert("It can't be a city of light");
+        // }
+        
+         //====Chapter 9 to 11 and Task2
+        // var gender=prompt("Enter Your Gender")
+        //   gender =gender.toLowerCase();
+        // if (gender==="male"){
+        //     alert("Good Morning Sir");
+        // }
+        // else{
+        //     alert("Good Morning Ma'am");
+        // }
+
+        //====Chapter 9 to 11 and Task3
+        // var col_Of_traafic_Signal=prompt("Enter Color")
+        // col_Of_traafic_Signal=col_Of_traafic_Signal.toLowerCase();
+        // if(col_Of_traafic_Signal==="red"){
+        //     alert(" MUST STOP");
+        // }
+        // else if( col_Of_traafic_Signal==="yellow"){
+        //      alert("READY TO MOVE");
+        //  }
+        //  else if( col_Of_traafic_Signal==="green"){
+        //     alert("MOVE NOW");
+        // }
+
+         //====Chapter 12 to 13 and Task1
+    //   var character=prompt("Enter a Number or Alphabet");
+    //   if(character>="A" && character<="Z"){
+    //       alert("Uppercase alphabet")
+    //   }
+    //   else if (character>="a" && character<="z"){
+    //       alert("Lowercase alphabet");
+    //   }
+    //   else if( character>=0 && character<=9)
+    //   {
+    //       alert ("Number");
+    //   }
+
+    //====Chapter 17 to 20 and Task4
+    // var number=prompt("Enter Table Number");
+    // var length=prompt("Enter Table Length")
+    //     if(number>0){
+    //         for( j=1; j<=length; j++){
+    //           document.write(number+ "x"+ j+ "=" + number*j);
+    //               document.write("<br>");
+    //          }
+    //         }
+
+    //====Chapter 17 to 20 and Task5
+
+    // var fruits=["apple", "banana", "mango", "orange", "strawbery"]
+    // for(i=0; i<fruits.length; i++){
+    //     document.write("Element at index"+" "+i+" "+ fruits[i] + "<br/>")
+    // }
+
+         
+            
+        
+        
+           
+           
+
+
+
+
+         
+
+         
+         
+
+
+
+      
